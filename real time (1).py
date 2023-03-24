43a418481285f751bbc63a37f77954f63a09c2a2
iremozcann <85320204+iremozcann@users.noreply.github.com>
2023-03-24 21:43:16 +0300

    Update real time (1).py

--- real time (1).py
+++ real time (1).py
@@ -1,4 +1,7 @@
 
+#real-time geometric shape counter using the OpenCV library. 
+#It detects the type of geometric shapes in real-time video input from a webcam and counts the number, perimeter, and area of each shape
+
 import math
 import numpy as np
 import cv2
